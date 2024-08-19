db992bfed33edb2d79b97a9e6c6549df31731908
Hernán Indíbil de la Cruz Calvo <indikalesken@hotmail.com>
2024-08-19 21:24:30 +0200

    Not forcing to use Futures in the API

--- src/main/java/com/clanjhoo/vampire/VampireAPI.java
+++ src/main/java/com/clanjhoo/vampire/VampireAPI.java
@@ -9,34 +9,82 @@ import org.jetbrains.annotations.NotNull;
 import java.util.concurrent.CompletableFuture;
 
 public class VampireAPI {
-    public static CompletableFuture<Boolean> isHealthy(@NotNull Player player) {
+    public static CompletableFuture<Boolean> isHealthyFuture(@NotNull Player player) {
         return VampireRevamp.syncTaskVPlayer(player, null, null)
                 .thenApply((VPlayer::isHealthy));
     }
 
-    public static CompletableFuture<Boolean> isUnhealthy(@NotNull Player player) {
+    public static CompletableFuture<Boolean> isUnhealthyFuture(@NotNull Player player) {
         return VampireRevamp.syncTaskVPlayer(player, null, null)
                 .thenApply((VPlayer::isUnhealthy));
     }
 
-    public static CompletableFuture<Boolean> isHuman(@NotNull Player player) {
+    public static CompletableFuture<Boolean> isHumanFuture(@NotNull Player player) {
         return VampireRevamp.syncTaskVPlayer(player, null, null)
                 .thenApply((VPlayer::isHuman));
     }
 
-    public static CompletableFuture<Boolean> isInfected(@NotNull Player player) {
+    public static CompletableFuture<Boolean> isInfectedFuture(@NotNull Player player) {
         return VampireRevamp.syncTaskVPlayer(player, null, null)
                 .thenApply((VPlayer::isInfected));
     }
 
-    public static CompletableFuture<Boolean> isVampire(@NotNull Player player) {
+    public static CompletableFuture<Boolean> isVampireFuture(@NotNull Player player) {
         return VampireRevamp.syncTaskVPlayer(player, null, null)
                 .thenApply((VPlayer::isVampire));
     }
 
-    public static CompletableFuture<Boolean> isNosferatu(@NotNull Player player) {
+    public static CompletableFuture<Boolean> isNosferatuFuture(@NotNull Player player) {
         return VampireRevamp.syncTaskVPlayer(player, null, null)
                 .thenApply((VPlayer::isNosferatu));
+    }
+
+    public static boolean isHealthy(@NotNull Player player) throws AssertionError {
+        VPlayer vPlayer = VampireRevamp.getVPlayerNow(player);
+        if (vPlayer == null) {
+            throw new AssertionError("Data not loaded yet");
+        }
+        return vPlayer.isHealthy();
+    }
+
+    public static boolean isUnhealthy(@NotNull Player player) throws AssertionError {
+        VPlayer vPlayer = VampireRevamp.getVPlayerNow(player);
+        if (vPlayer == null) {
+            throw new AssertionError("Data not loaded yet");
+        }
+        return vPlayer.isUnhealthy();
+    }
+
+    public static boolean isHuman(@NotNull Player player) throws AssertionError {
+        VPlayer vPlayer = VampireRevamp.getVPlayerNow(player);
+        if (vPlayer == null) {
+            throw new AssertionError("Data not loaded yet");
+        }
+        return vPlayer.isHuman();
+    }
+
+    public static boolean isInfected(@NotNull Player player) throws AssertionError {
+        VPlayer vPlayer = VampireRevamp.getVPlayerNow(player);
+        if (vPlayer == null) {
+            throw new AssertionError("Data not loaded yet");
+        }
+        return vPlayer.isInfected();
+    }
+
+    public static boolean isVampire(@NotNull Player player) throws AssertionError {
+        VPlayer vPlayer = VampireRevamp.getVPlayerNow(player);
+        if (vPlayer == null) {
+            throw new AssertionError("Data not loaded yet");
+        }
+        return vPlayer.isVampire();
+    }
+
+    public static boolean isNosferatu(@NotNull Player player) throws AssertionError {
+        VPlayer vPlayer = VampireRevamp.getVPlayerNow(player);
+        if (vPlayer == null) {
+            throw new AssertionError("Data not loaded yet");
+        }
+        return vPlayer.isNosferatu();
     }
 
     public static boolean isSunRing(@NotNull ItemStack item) {
